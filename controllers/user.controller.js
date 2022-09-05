e01313aabc0040d28e51908ff4616d4b8100d742
ImranRH321 <imranrh247@gmail.com>
2022-09-05 18:06:53 +0600

    all-user-and-limit

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,23 +4,13 @@ const path = require("path");
 let data = fs.readFileSync("./userData.json", "utf-8");
 const userData = JSON.parse(data);
 
-/* fs.readFile("./public/userData.json", 'utf-8', (err, data) => {
-  if(err){
-    console.log(err.message);
-  }
-  else{
-      console.log(JSON.parse(data));
-  }
-}) */
-
 /* home user home route */
 module.exports.userHome = (req, res, next) => {
-  res.send("<h1>WelCome to Random User Assignment here</h1>");
-  // res.sendFile(path.join(__dirname + "../views/saveForm.html"));
+  res.send("<h1>Random User Assignment !</h1>");
   next();
 };
 
-// random user data
+/* Random user Data */
 module.exports.randomUser = (req, res, next) => {
   const random = Math.floor(Math.random() * 5);
   const oneData = userData.find(el => el.id == random);
@@ -28,48 +18,25 @@ module.exports.randomUser = (req, res, next) => {
   next();
 };
 
-// all user data
-// limit implement korbe
+/* All User Data */
 module.exports.userAll = (req, res, next) => {
-  if (req.query.name) {
-    const searchName = req.query.name.toLowerCase();
-    const matchedName = userData.filter(user =>
-      user.name.toLowerCase().includes(searchName)
-    );
-    res.send(matchedName);
-  } else if (req.query.id) {
-    const searchId = parseInt(req.query.id);
-    const matchedId = userData.filter(user => user.id == searchId);
-    res.send(matchedId);
-  } else if (req.query.contact) {
-    const searchContact = req.query.contact.toLowerCase();
-    const matchedContact = userData.filter(user =>
-      user.contact.toLowerCase().includes(searchContact)
-    );
-    res.send(matchedContact);
-  } else if (req.query.address) {
-    const searchAddress = req.query.address.toLowerCase();
-    const matchedAddress = userData.filter(user =>
-      user.address.toLowerCase().includes(searchAddress)
-    );
-    res.send(matchedAddress);
-  } else if (req.query.photoUrl) {
-    const searchPhotoUrl = req.query.photoUrl;
-    const matchedPhotoUrl = userData.filter(user =>
-      user.photoUrl.includes(searchPhotoUrl)
-    );
-    res.send(matchedPhotoUrl);
+  const { limit } = req.query;
+  if (limit) {
+    const allDataLimit = userData.slice(0, limit);
+    res.send(allDataLimit);
+  } else {
+    res.send(userData);
   }
-  res.send(userData);
   next();
 };
 
 module.exports.userSave = (req, res, next) => {
   const fromData = req.body;
+  console.log(fromData);
   const newUserAdd = [...userData, fromData];
   const stringFyUser = JSON.stringify(newUserAdd);
   fs.writeFileSync("./userData.json", stringFyUser);
-  res.send("home");
+  res.send("save");
   /* 
   check duplicate data 
   err status code 
@@ -139,6 +106,7 @@ module.exports.userBulkUpdate = (req, res, next) => {
   next();
 };
 
+/* Delete User */
 module.exports.userDelete = (req, res, next) => {
   const id = parseInt(req.params.id);
   const fileData = fs.readFileSync("./userData.json", "utf-8");
@@ -160,4 +128,5 @@ module.exports.userDelete = (req, res, next) => {
       success: true,
     });
   }
+  next();
 };
